480ba1d0c93dc9e7a8ec45121238d048d01bf9fd
hailxo <hailxl@gmail.com>
2022-02-24 11:14:10 +0800

    leetcode move zeros 相对高效的实现

--- python/src/leetcode/begin_algorithm/move_zeros.py
+++ python/src/leetcode/begin_algorithm/move_zeros.py
@@ -8,22 +8,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # 从数组头开始查找 0 元素，找到 0 元素后则把从这个 0 元素后一位直到数组末尾的所有数字向前移 1 位，
-        # 然后把 0 插到队列末尾
-        # 继续从原 0 位置向后查找新的 0 元素，该位置不是 0 则向后查找 1 位
-        # 重复这个过程
-        # 怎么知道已经处理完所有 0 元素？用一个计数器记录已经移动的 0 元素个数，
-        # 已经移动的个数加上当前处理到的位置应该等于数组最大索引
-
-        current_index = 0
+        # 从头遍历数组，记录当前有多少个 0 元素 k，把当前非 0 元素向前移动 k 位，最后把数组末尾 k 个位置补上 0
         zero_count = 0
-        while current_index + zero_count != len(nums):
-            if nums[current_index] == 0:
-                nums[current_index:-1] = nums[current_index+1:]
-                nums[-1] = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
                 zero_count += 1
             else:
-                current_index += 1
+                nums[i - zero_count] = nums[i]
+        nums[len(nums) - zero_count: ] = (0 for _ in range(zero_count))
 
 
 l = [0, 1, 0, 3, 12]
