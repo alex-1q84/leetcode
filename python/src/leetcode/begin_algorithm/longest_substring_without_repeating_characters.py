f5e7ccc07ad2520adebe5d9ce7ff7e8dd648acbd
hailxo <hailxl@gmail.com>
2022-02-28 18:57:18 +0800

    leetcode longest_substring_without_repeating_characters 用字典存储字母最后出现位置，节省窗口内重复字母查找时间

--- python/src/leetcode/begin_algorithm/longest_substring_without_repeating_characters.py
+++ python/src/leetcode/begin_algorithm/longest_substring_without_repeating_characters.py
@@ -2,27 +2,20 @@
 # https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 # 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # 滑动窗口
-        # 窗口左侧和右侧位置为 -1 ，当下一个字母没有出现走窗口内，则窗口右边框向右滑动 1 ，
+        # 窗口左侧位置为 -1 ，当下一个字母没有出现走窗口内，则窗口右边框向右滑动 1 ，
         # 否则左侧边框滑动到第一个重复字母位置，右边框向右滑动 1 （包含当前位置字母），
         # 如此循环，直到右边框位置到达字符串末尾
-        left, right = -1, -1
+        # PS: 因为右侧边框总是在当前字母位置，其实可以用当前字母位置代表右边框，从而节省一个变量
+        left = -1
         max_width = 0
-
-        def pos_in_window(c):
-            for n in range(left+1, right+1):
-                if s[n] == c:
-                    return n
-            return -1
+        last_poses = {}
 
         for i in range(len(s)):
-            pos = pos_in_window(s[i])
-            if pos == -1:
-                right += 1
-                max_width = max(max_width, (right - left))
-            else:
-                left = pos
-                right += 1
+            left = max(last_poses.get(s[i], -1), left)
+            max_width = max(max_width, (i - left))
+            last_poses[s[i]] = i
         return max_width
