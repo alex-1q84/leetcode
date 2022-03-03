0e73c3da8cf79c1b138d762c34179d71c3eec6e1
hailxo <hailxl@gmail.com>
2022-03-03 14:51:01 +0800

    leetcode reverse_linked_list 去掉多余的赋值语句

--- python/src/leetcode/begin_algorithm/reverse_linked_list.py
+++ python/src/leetcode/begin_algorithm/reverse_linked_list.py
@@ -9,12 +9,11 @@ class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         # 1. 把当前节点设置为新链表的头
         # 2. 取出节点的下一个节点
-        # 3. 如此往复，知道当前节点为 None
+        # 3. 如此往复，直到当前节点为 None
         new_link = None
         current = head
         while current != None:
             n = current.next
-            current.next = None
             current.next = new_link
             new_link = current
             current = n
